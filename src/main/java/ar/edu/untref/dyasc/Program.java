a77acd71f743eb6d98db45dd80c1731388db0dc2
gabrielbosio <gabrielbosio95@gmail.com>
2018-11-01 14:55:44 -0300

    Test agregado

--- src/main/java/ar/edu/untref/dyasc/Program.java
+++ src/main/java/ar/edu/untref/dyasc/Program.java
@@ -4,8 +4,11 @@ public class Program {
 
     public static void main(String[] args) {
     
-        System.out.println("Hola a todos!!!")
+        System.out.println("Hola a todos!!!");
     
     }
 
+    public static int cuadradoDe(int x) {
+        return x * x;
+    }
 }
---
+++ src/test/java/ar/edu/untref/dyasc/ProgramTest.java
@@ -0,0 +1,14 @@
+package ar.edu.untref.dyasc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProgramTest {
+
+    @Test
+    public void test() {
+        assertEquals(3, Program.cuadradoDe(2));
+    }
+
+}
